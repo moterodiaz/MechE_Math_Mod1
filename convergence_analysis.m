ab0a1093e5e116dcebe3dc0246d212fcdc6afe96
Fil-kyp12 <157530101+Fil-kyp12@users.noreply.github.com>
2026-09-03 10:07:47 -0400

    Convergence Analysis and Data Collection Functions

---
+++ convergence_analysis.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = convergence_analysis(inputArg1,inputArg2)
+%convergence_analysis Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
---
+++ data_collection.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = data_collection(inputArg1,inputArg2)
+% Given a root-finding method, 
+% UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
